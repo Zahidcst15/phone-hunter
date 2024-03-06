ce83f0dbccd3cb91e1e89cd980e6eba9783bac85
Zahidcst15 <zahid.cst15@gmail.com>
2024-03-07 03:32:48 +0600

    add a modal and set mobile details

--- js/phone.js
+++ js/phone.js
@@ -32,8 +32,8 @@ const displayPhones = (phones, isShowAll) =>{
         <div class="card-body">
           <h2 class="card-title">${phone.phone_name}</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+          <div class="card-actions justify-center">
+            <button onClick="handelShowDetail('${phone.slug}'); my_modal_5.showModal()" class="btn btn-primary">Show Details</button>
           </div>
         </div>
         `;
@@ -42,6 +42,37 @@ const displayPhones = (phones, isShowAll) =>{
     // hide loading spinner
     toggleLoadingSpinner(false);
 }
+
+const handelShowDetail = async (id) =>{
+      //  load single phone data
+      const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+      const data = await res.json();
+      const phone = data.data;
+
+      showPhoneDetails(phone)
+
+}
+
+
+const showPhoneDetails =(phone) =>{
+  const phoneName = document.getElementById('phone-name')
+  phoneName.innerText = phone.name
+
+
+  // show the modal
+  my_modal_5.showModal()
+}
+
+
+
+
+
+
+
+
+
+
+
 
 // handel search button
 const handelSearch = (isShowAll) =>{
